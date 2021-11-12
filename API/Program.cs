6ba69c7a86be6a2fb6839aaa70be4c81ffefa7a0
LDVES <szymonwojcik800@gmail.com>
2021-11-12 22:53:37 +0100

    Add SSL to Angular

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<AppDataContext>(options => {
 });
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +25,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(options => {
+    options.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
 
 app.UseAuthorization();
 
